def691c240a684312c78276e65e54da9c1e64488
Susan <bmoregarfield@gmail.com>
2025-03-19 18:25:50 -0400

    Added Skip Back functionality and added working websites in the readme. Also made a special case for HBO Max.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# [Insert README here]
+# Working Sites
+- Hulu  
+- Netflix   
+- Peacock   
+- Apple TV+     
+- HBO Max
+
+# Not Working Sites
+  - Amazon Prime
--- src/content-script.js
+++ src/content-script.js
@@ -1,12 +1,12 @@
 document.addEventListener('keydown', (event) => {
+    // Gets video player element
+    const videoElement = document.querySelector('video');
+
     switch (event.key) {
         case ('l' || 'L'):
             // Prevent default event
             event.preventDefault();
-        
-            // Gets video player
-            const videoElement = document.querySelector('video');
-        
+
             if (videoElement) {
                 // Encodes a new keyboard event, mimicking a right-arrow key event
                 const rightArrowEvent = new KeyboardEvent('keydown', {
@@ -17,17 +17,38 @@ document.addEventListener('keydown', (event) => {
                     bubbles: true,
                     cancelable: true
                 });
-            
-                console.log('L key pressed, dispatching right arrow event');
+
                 // And then we dispatch the event to the page
-                videoElement.dispatchEvent(rightArrowEvent);
+                if (window.location.href.substring(0, 20) == "https://play.max.com") {
+                    document.querySelector('[data-testid="player-ux-skip-forward-button"]').click();
+                } else {
+                    videoElement.dispatchEvent(rightArrowEvent);
+                }
+
             }
             break;
         case ('j' || 'J'):
-            break;
-        case ('i' || 'I'):
-            break;
-        case ('k' || 'K'):
+            // Prevent default event
+            event.preventDefault();
+        
+            if (videoElement) {
+                // Encodes a new keyboard event, mimicking a left-arrow key event
+                const leftArrowEvent = new KeyboardEvent('keydown', {
+                    key: 'ArrowLeft',
+                    code: 'ArrowLeft',
+                    keyCode: 37, // left arrow keycode
+                    which: 37,
+                    bubbles: true,
+                    cancelable: true
+                });
+
+                // And then we dispatch the event to the page
+                if (window.location.href.substring(0, 20) == "https://play.max.com") {
+                    document.querySelector('[data-testid="player-ux-skip-back-button"]').click();
+                } else {
+                    videoElement.dispatchEvent(leftArrowEvent);
+                }
+            }
             break;
     }
 });
